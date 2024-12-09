9f7f2ba81b40af2801ed6a5e33a215109019f4c4
Stalkerep <enzo.agautbonnet@gmail.com>
2024-12-09 15:34:59 +0100

    ajout de stripe 2

--- products.py
+++ products.py
@@ -4,4 +4,5 @@
 
 PRODUCTS = [
     {"id": 1, "name": "Produit 1", "price": 2300},  # 23,00 €
-    {"id": 2, "name": "Produit 2", "price": 4800},]  # 48,00 € 
+    {"id": 2, "name": "Produit 2", "price": 4800},  # 48,00 €
+]
